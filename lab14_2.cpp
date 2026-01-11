eb5e623212c679753c080b892430465430fb5745
paphawinkhet-star <paphawin.khet@gmail.com>
2026-01-11 16:02:45 +0700

    complete README.md lab14_1.cpp lab14_2.cpp

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -3,11 +3,41 @@ using namespace std;
 
 const int N = 5;
 
-void inputMatrix(double [][N]);
+void inputMatrix(double A[N][N]){
+    for(int i = 0; i < N; i++){
+        cout << "Row " << i + 1 << ": ";
+        for(int j = 0; j < N; j++){
+            cin >> A[i][j];
+        }
+    }
+}
 
-void findLocalMax(const double [][N], bool [][N]);
+void findLocalMax(double A[N][N], bool B[N][N]){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            // ขอบ matrix = false
+            if(i == 0 || i == N-1 || j == 0 || j == N-1){
+                B[i][j] = false;
+            }
+            else{
+                B[i][j] =
+                    A[i][j] >= A[i-1][j] &&   // บน
+                    A[i][j] >= A[i+1][j] &&   // ล่าง
+                    A[i][j] >= A[i][j-1] &&   // ซ้าย
+                    A[i][j] >= A[i][j+1];     // ขวา
+            }
+        }
+    }
+}
 
-void showMatrix(const bool [][N]);
+void showMatrix(bool B[N][N]){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cout << B[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main(){
 	double A[N][N]; 
